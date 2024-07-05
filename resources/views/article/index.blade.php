5b4115ccf918970e34f927ce8dfb41b685adb3ea
softcommteam2 <61075527+softcommteam2@users.noreply.github.com>
2024-07-05 14:56:33 +0630

    hha

---
+++ resources/views/article/index.blade.php
@@ -0,0 +1,11 @@
+@extends("layouts.app");
+@section('content')
+ <div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h3>Article List</h3>
+        </div>
+    </div>
+ </div>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+Route::middleware(['auth'])->group(function(){
+    Route::resource("article",ArticleController::class);
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+});
+
